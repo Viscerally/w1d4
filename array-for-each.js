c2c0a0b81eabb1f3138816346643a18a523546e7
Kevin Paniccia <kevin.paniccia@gmail.com>
2018-05-10 21:37:01 +0000

    refactored to use a method instead of a for Loop

--- array-for-each.js
+++ array-for-each.js
@@ -1,15 +1,19 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach((value, index)=> {
+    if (value === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  })
 }
+
 
 function actionWhenFound(index) {
   console.log("Found Waldo at index: " + index);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
+var array1 = ["Alice", "Bob", "Waldo", "Winston"];
+// array1.forEach((value, index)=> {
+  // console.log(value, index);
+// });
